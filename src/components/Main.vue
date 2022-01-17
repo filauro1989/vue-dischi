78eab028d05818c12bc7132c8c1e5c9d9ed21fd2
filauro1989 <filauro1989@gmail.com>
2022-01-17 18:04:16 +0100

    bonus

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,6 @@
 <template>
         <div class="container-fluid bg-color" >
-            <div class="w-60 centered pt-6">
+            <div v-if="cards" class="w-60 centered pt-6">
                 <div class="row row-cols-5">
                     <!-- <div class="col text-center text-light px-3">
                         <div class="card-wrapper card-bg-color p-3">
@@ -13,6 +13,10 @@
                     <Cards v-for="(card, index) in cards" :key="index +card.id" :image="card.poster" :title="card.title" :author="card.author" :year="card.year">
                     </Cards>
                 </div>
+            </div>
+            <div class="w-60 centered pt-6" v-else>
+                <h1 class="loading text-light">Loading</h1>
+                <div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
             </div>
         </div>
 </template>
@@ -33,9 +37,11 @@ export default {
         }
     },
     created() {
-    this.getCard();
-    },
+        },
     mounted() {
+        setTimeout(() => {
+            this.getCard();
+        }, 3000);
     
     },
     methods: {
@@ -62,5 +68,83 @@ export default {
         height: calc(100vh - 50px - 1.4em);
         width: 100vw;
     };
+    .lds-spinner {
+  color: official;
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+}
+.lds-spinner div {
+  transform-origin: 40px 40px;
+  animation: lds-spinner 1.2s linear infinite;
+}
+.lds-spinner div:after {
+  content: " ";
+  display: block;
+  position: absolute;
+  top: 3px;
+  left: 37px;
+  width: 6px;
+  height: 18px;
+  border-radius: 20%;
+  background: #fff;
+}
+.lds-spinner div:nth-child(1) {
+  transform: rotate(0deg);
+  animation-delay: -1.1s;
+}
+.lds-spinner div:nth-child(2) {
+  transform: rotate(30deg);
+  animation-delay: -1s;
+}
+.lds-spinner div:nth-child(3) {
+  transform: rotate(60deg);
+  animation-delay: -0.9s;
+}
+.lds-spinner div:nth-child(4) {
+  transform: rotate(90deg);
+  animation-delay: -0.8s;
+}
+.lds-spinner div:nth-child(5) {
+  transform: rotate(120deg);
+  animation-delay: -0.7s;
+}
+.lds-spinner div:nth-child(6) {
+  transform: rotate(150deg);
+  animation-delay: -0.6s;
+}
+.lds-spinner div:nth-child(7) {
+  transform: rotate(180deg);
+  animation-delay: -0.5s;
+}
+.lds-spinner div:nth-child(8) {
+  transform: rotate(210deg);
+  animation-delay: -0.4s;
+}
+.lds-spinner div:nth-child(9) {
+  transform: rotate(240deg);
+  animation-delay: -0.3s;
+}
+.lds-spinner div:nth-child(10) {
+  transform: rotate(270deg);
+  animation-delay: -0.2s;
+}
+.lds-spinner div:nth-child(11) {
+  transform: rotate(300deg);
+  animation-delay: -0.1s;
+}
+.lds-spinner div:nth-child(12) {
+  transform: rotate(330deg);
+  animation-delay: 0s;
+}
+@keyframes lds-spinner {
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
 
 </style>
