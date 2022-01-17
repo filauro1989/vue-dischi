8412b9b3da13216094b686897183f4a24f8694dc
filauro1989 <filauro1989@gmail.com>
2022-01-17 16:20:59 +0100

    compiling Main

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container bg-color">
+        <img src="../assets/spotify-logo.png" alt="">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+    @import "../assets/scss/partials/_variables.scss";
+    @import "../assets/scss/partials/_commons.scss"; 
+
+
+.bg-color {
+    background-color: $cardBackground;
+}
+.container {
+    img {
+        height: 50px;
+        margin: 0.7em 0;
+        margin-left: 1.8em;
+    }
+}
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,31 @@
+<template>
+        <div class="container bg-color" >
+            <div class="w-60 centered">
+                asd
+            </div>
+        </div>
+</template>
+
+<script>
+export default {
+    name: 'Main',
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+    @import "../assets/scss/partials/_variables.scss"; 
+    @import "../assets/scss/partials/_commons.scss"; 
+
+    .bg-color {
+        background-color: $backgroundColor;
+    };
+    .container {
+        height: calc(100vh - 50px - 1.65em);
+        width: 100vw;
+    }
+</style>
